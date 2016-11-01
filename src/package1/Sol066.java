d2fe171e4f53faf5c85d5ca753be75b9ee4e01f8
wyun <wyawxy@gmail.com>
2016-11-01 00:49:31 +0000

    digit

---
+++ src/package1/Sol066.java
@@ -0,0 +1,29 @@
+package package1;
+
+public class Sol066 {
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        int a = 1;
+        
+        for(int i = l - 1; i>=0; i--){
+            if(digits[i]+a==10){
+                digits[i] = 0;
+            }
+            else{
+                digits[i]+=a;
+                a = 0;
+            }
+        }
+        
+        if (a == 1){
+            int[] r = new int[l+1];
+            r[0] = 1;
+            for(int m = 1; m<l+1; m++){
+                r[m] = 0;
+            }
+            return r;
+        }
+        
+        return digits;
+    }
+}
