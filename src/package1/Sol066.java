01df5207a3014dfd61c2583cffedc29b50a218e8
wyun <wyawxy@gmail.com>
2016-11-01 20:29:36 +0000

    comments

--- src/package1/Sol066.java
+++ src/package1/Sol066.java
@@ -18,7 +18,7 @@ public class Sol066 {
         if (a == 1){ // if 9999...
             int[] r = new int[l+1];
             r[0] = 1;
-            for(int m = 1; m<l+1; m++){
+            for(int m = 1; m<l+1; m++){ //from r[1] to r[l]
                 r[m] = 0;
             }
             return r;
