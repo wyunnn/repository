13392fa01275b9c44edb0c4f00552d51ea2502ce
wyun <wyawxy@gmail.com>
2016-11-02 13:26:01 +0000

    linked list

--- src/package1/Sol24.java
+++ src/package1/Sol24.java
@@ -1,5 +1,26 @@
 package package1;
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class Sol24 {
-
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)   return head;
+        
+        ListNode n1 = head;
+        ListNode n2 = head.next;
+        ListNode n3 = head.next.next;
+        
+        head = head.next;
+        n2.next = n1;
+        n1.next = swapPairs(n3);
+        
+        return head;
+        
+    }
 }
