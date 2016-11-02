12fcd623eb7f05719e72102c7646a869089b670e
wyun <wyawxy@gmail.com>
2016-11-02 13:46:33 +0000

    ^^^^^^^^^^^^^

--- src/package1/Sol136.java
+++ src/package1/Sol136.java
@@ -1,5 +1,12 @@
 package package1;
 
 public class Sol136 {
-
+    public int singleNumber(int[] nums) {
+        int r = 0;
+        
+        for (int i = 0; i < nums.length; i++){
+            r = r ^ nums[i];
+        }
+        return r;
+    }
 }
