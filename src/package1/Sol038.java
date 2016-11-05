300ac0a5595b08f87905ea0e88275e92c9520055
wyun <wyawxy@gmail.com>
2016-11-05 15:25:35 +0000

    string

---
+++ src/package1/Sol038.java
@@ -0,0 +1,29 @@
+package package1;
+
+public class Sol038 {
+    public String countAndSay(int n) {
+        if(n == 0)  return null;
+        String s = "1";
+        String rs;
+        
+        for (int i = 1; i < n; i++){
+            rs = new String();
+            char a = s.charAt(0);
+            int count = 1;
+            
+            for(int j = 1; j < s.length(); j++){
+                char cc = s.charAt(j);
+                if(cc == a){
+                    count++;
+                }
+                else{
+                    rs = rs + count + a;
+                    a = cc;
+                    count = 1;
+                }
+            }
+            s = rs + count + a;
+        }
+        return s;
+    }
+}
