937573642b44031b38229839f86e0323dfd91c63
wyun <wyawxy@gmail.com>
2016-11-06 19:02:23 +0000

    I don't want to solve this question..

--- src/package1/Sol008.java
+++ src/package1/Sol008.java
@@ -9,20 +9,16 @@ public class Sol008 {
         
         for (int i = 0; i < charArray.length; i++){
             a = charArray[i];
-            if(a == '-' && sign == 0)               sign = -1;
-            else if (a == '+' && sign == 0)         sign = 1;
+            if(a == '-' && sign == 0)               	sign = -1;
+            else if (a == '+' && sign == 0)         	sign = 1;
             else if('0' <= a && a <= '9'){
                 if(sign == 0)   sign = 1;
                 output = output * 10 + (a - '0');
-                if(sign * output > Integer.MAX_VALUE)       return Integer.MAX_VALUE;
-                if(sign * output < Integer.MIN_VALUE)       return Integer.MIN_VALUE;
+                if(sign * output > Integer.MAX_VALUE)   return Integer.MAX_VALUE;
+                if(sign * output < Integer.MIN_VALUE)   return Integer.MIN_VALUE;
             }
-            else if(a == ' '){
-                if(sign != 0)
-                    break;
-            }
-            else
-                break;
+            else if(a == ' ' && sign != 0)              break;
+            else							            break;
         }
         return sign * (int)output;
     }
