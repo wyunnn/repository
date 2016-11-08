fa1b750db133cff32bb208c26d116806e048ce32
wyun <wyawxy@gmail.com>
2016-11-08 16:57:55 +0000

    math question

--- src/package1/Sol009.java
+++ src/package1/Sol009.java
@@ -1,5 +1,27 @@
 package package1;
 
 public class Sol009 {
+    public boolean isPalindrome(int x) {
+        int right, left;
+        int dl = 1;
+        int dr = 1;
+        
+        if(x<0)    return false;
+        
+        while(x/dl >= 10){
+            dl*=10;
+        }
+        
+        do {
+            right = (x / dr) % 10;
+            left = (x / dl) % 10;
+            dl = dl/10;
+            dr = dr*10;
+        
+        } while(right == left && dl / dr >= 10);
+        
+        if(left == right)   return true;
+        else                return false;
 
+    }
 }
