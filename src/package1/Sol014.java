c1b0ddb92f25c9bc374a1cfa832ba73b634b2664
wyun <wyawxy@gmail.com>
2016-11-08 17:32:45 +0000

    string search

---
+++ src/package1/Sol014.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class Sol014 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)    return "";
+        
+        int i,j;
+        int ind = strs[0].length();
+
+        for( i = 1; i < strs.length; i++){
+            for(j = 0; j < strs[i].length() && j < strs[0].length(); j++){
+                if(strs[0].charAt(j) != strs[i].charAt(j)){
+                    break;
+                }
+            }
+            if (j < ind)		ind = j;
+        }
+        
+        return strs[0].substring(0, ind);
+    }
+}
