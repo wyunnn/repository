7236c966c4067feab5d65c7754c56c961c9fc49d
wyun <wyawxy@gmail.com>
2016-11-11 15:07:34 +0000

    product@@

--- src/package1/Sol238.java
+++ src/package1/Sol238.java
@@ -3,7 +3,63 @@ package package1;
 import java.util.*;
 
 public class Sol238 {
+	// O(n) time and O(1) space
     public int[] productExceptSelf(int[] nums) {
+        int [] a = new int[nums.length];
+        int [] b = new int[nums.length];
+        
+        Arrays.fill(a, 1);
+        Arrays.fill(b, 1);
+
+        
+        for(int i = 1; i < nums.length; i++){
+            a[i] = a[i-1]*nums[i-1];
+        }
+        
+        int p = 1;
+        for(int i = nums.length - 1; i >= 0; i--){
+            a[i] = a[i]*p;
+            p = p*nums[i];
+        }
+        
+        return a;
+        
+    }
+	
+	
+/*
+ *  With extra space O(n)
+ *  O(n)
+ *  
+ * 	public int[] productExceptSelf(int[] nums) {
+        int [] a = new int[nums.length];
+        int [] b = new int[nums.length];
+        
+        Arrays.fill(a, 1);
+        Arrays.fill(b, 1);
+
+        
+        for(int i = 1; i < nums.length; i++){
+            a[i] = a[i-1]*nums[i-1];
+        }
+        
+        for(int i = nums.length - 2; i >= 0; i--){
+            b[i] = b[i+1]*nums[i+1];
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            a[i] = b[i]*a[i];
+        }
+        return a;
+        
+    }*/
+	
+	
+	
+/*
+ *		O(n^2)
+ *     
+ *     public int[] productExceptSelf(int[] nums) {
         int [] a = new int[nums.length];
         Arrays.fill(a, 1);
         
@@ -14,5 +70,5 @@ public class Sol238 {
         }
         return a;
         
-    }
+    }*/
 }
