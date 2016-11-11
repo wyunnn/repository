67df3f2803c2c4b9810243cc1cea662d83f4f9f4
wyun <wyawxy@gmail.com>
2016-11-11 15:08:13 +0000

    product

--- src/package1/Sol238.java
+++ src/package1/Sol238.java
@@ -6,11 +6,7 @@ public class Sol238 {
 	// O(n) time and O(1) space
     public int[] productExceptSelf(int[] nums) {
         int [] a = new int[nums.length];
-        int [] b = new int[nums.length];
-        
         Arrays.fill(a, 1);
-        Arrays.fill(b, 1);
-
         
         for(int i = 1; i < nums.length; i++){
             a[i] = a[i-1]*nums[i-1];
