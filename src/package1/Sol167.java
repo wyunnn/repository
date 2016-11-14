e5d3c722b8b958f08fb34d05419679d5bc13884f
wyun <wyawxy@gmail.com>
2016-11-14 21:56:00 +0000

    pointer and search

---
+++ src/package1/Sol167.java
@@ -0,0 +1,25 @@
+package package1;
+
+public class Sol167 {
+    public int[] twoSum(int[] numbers, int target) {
+        int [] ans = new int[2];
+        int i = 0;
+        int j = numbers.length - 1;
+        
+        while(i<j){
+            if (numbers[i]+numbers[j] > target){
+                j--;
+            }
+            else if (numbers[i]+numbers[j] < target){
+                i++;
+            }
+            else{
+                ans[0] = i+1;
+                ans[1] = j+1;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+}
