f16e7d67a92774382f9497a2d2a66f551a39ac47
wyun <wyawxy@gmail.com>
2016-11-23 19:19:52 +0000

    omg i don't understand

---
+++ src/package1/Sol3940.java
@@ -0,0 +1,48 @@
+package package1;
+
+import java.util.*;
+
+public class Sol3940 {
+    public String decodeString(String s) {
+        
+        Stack<String> ss = new Stack<String>();
+        Stack<Integer> si = new Stack<Integer>();
+        int n = 0; 
+        int num = 0;
+        String tmp = "";
+        String part = "";
+        for(char a : s.toCharArray()){
+            if(a >= '0' && a <= '9'){
+                num = num *10 + (a - '0');
+            }
+            else if (a == '['){
+                si.push(num);
+                num = 0;
+                ss.push(tmp);
+                tmp = "";
+            }
+            else if(a != '[' && a != ']'){
+                tmp = tmp + a; //get the letters inside 
+            }
+            else if (a == ']'){
+                if(si.size() >0){
+                    n = si.pop();
+                }
+                
+                part = "";
+                if(tmp.equals("")){
+                    tmp = ss.pop();
+                }
+                while(n > 0){
+                    part = part + tmp;
+                    n--;
+                }
+                
+                if(ss.size() > 0){
+                    tmp = ss.pop() + part;
+                }
+            }
+        }
+        return tmp;
+    }
+}
