442d940e30e66d69103378a1060944319caea4d1
wyun <wyawxy@gmail.com>
2016-12-03 22:22:34 +0000

    update

--- src/package1/Sol418.java
+++ src/package1/Sol418.java
@@ -3,8 +3,33 @@ package package1;
 public class Sol418 {
 	
 	
-	
-	
+	/*
+	 * similar to the solution 1 in this blog
+	 * http://www.cnblogs.com/grandyang/p/5975426.html
+	 */
+	public int wordsTyping(String[] sentence, int rows, int cols) {
+        String all = "";
+        int res = 0;
+        
+        for(int i = 0; i < sentence.length; i++){
+            all += sentence[i] + ' ';
+        }
+        int len = all.length();
+        
+        for(int i = 0; i < rows; i++){
+            res += cols;
+            if(all.charAt(res%len) == ' '){
+                res++;
+            }
+            else{
+                while(res > 0 && all.charAt(res%len) != ' '){
+                    res--;
+                }
+                res++;
+            }
+        }
+        return res / len;
+    }
 	
     /*
      * TLE
