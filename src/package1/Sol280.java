64bbf1bb8a9cf3f9696f99bb1bb992e4618f8a7b
wyun <wyawxy@gmail.com>
2016-12-17 23:34:11 +0000

    sol280

---
+++ src/package1/Sol280.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Sol280 {
+
+    public void wiggleSort(int[] nums) {
+        for (int i=1; i<nums.length; i++) {
+            int a = nums[i-1];
+            if ((i%2 == 1) == (a > nums[i])) {
+                nums[i-1] = nums[i];
+                nums[i] = a;
+            }
+        }
+    }
+}
