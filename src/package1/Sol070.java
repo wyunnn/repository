8514c6bb1c7284d6bd4a2eb241f11a07c8b3a459
wyun <wyawxy@gmail.com>
2017-01-11 20:49:04 +0800

    fibonacci

---
+++ src/package1/Sol070.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Sol070 {
+    public int climbStairs(int n) {
+        int[] step = new int[n+1];
+        step[0] = 1;
+        step[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            step[i] = step[i-1] + step[i-2];
+        }
+        return step[n];
+    }
+}
