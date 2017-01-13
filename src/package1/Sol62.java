5e9aae9c6fe64ba8122f06fd875c1e89e85e83a8
wyun <wyawxy@gmail.com>
2017-01-13 16:49:34 +0800

    m*n dynamic programming

---
+++ src/package1/Sol62.java
@@ -0,0 +1,22 @@
+package package1;
+
+import java.util.*;
+
+public class Sol62 {
+    public int uniquePaths(int m, int n) {
+        int[][] sum = new int[m][n];
+        Arrays.fill(sum[0], 1);
+        
+        for(int i = 1; i < m; i++){
+            sum[i][0] = 1;
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                sum[i][j] = sum[i-1][j]+sum[i][j-1];
+            }
+        }
+        
+        return sum[m-1][n-1];
+    }
+}
