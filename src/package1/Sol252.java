bff1badef7b621d3362febf66ee1e88df5cbec56
wyun <wyawxy@gmail.com>
2017-02-09 16:55:54 +0800

    Meeting Rooms

---
+++ src/package1/Sol252.java
@@ -0,0 +1,29 @@
+package package1;
+
+import java.util.*;
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Sol252 {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        
+        Arrays.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval a, Interval b){
+                return a.start - b.start;
+            }
+        });
+        
+        for (int i = 0; i < intervals.length-1; i++){
+            if(intervals[i].end > intervals[i+1].start){
+                return false;
+            }
+        }
+        return true;
+    }
+}
