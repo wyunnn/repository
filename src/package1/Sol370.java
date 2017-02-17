810e56e218ae41602b45ecfbac1ea811f266593b
wyun <wyawxy@gmail.com>
2017-02-17 13:24:22 +0800

    Range Addition

--- src/package1/Sol370.java
+++ src/package1/Sol370.java
@@ -1,5 +1,20 @@
 package package1;
 
 public class Sol370 {
-
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[] res = new int[length];
+        
+        for(int i = 0; i < updates.length; i++){
+            res[updates[i][0]]+=updates[i][2];
+            if(updates[i][1]+1<length){
+                res[updates[i][1]+1]-=updates[i][2];
+            }
+        }
+        
+        for(int i = 1; i < res.length; i++){
+            res[i] = res[i]+res[i-1];
+        }
+        
+        return res;
+    }
 }
