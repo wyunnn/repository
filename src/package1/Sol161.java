224867ba24575081fecb574201766a5b8054a7e2
wyun <wyawxy@gmail.com>
2017-02-28 21:04:12 +0800

    One Edit Distance

---
+++ src/package1/Sol161.java
@@ -0,0 +1,27 @@
+package package1;
+
+public class Sol161 {
+	
+	
+	
+	
+	
+/*  
+ *  Time limit Exceeded
+ * 
+ *   public boolean isOneEditDistance(String s, String t) {
+        
+        if (s.length() == 0 || t.length() == 0){
+            if(Math.abs(s.length() - t.length()) == 1)
+                return true;
+            else
+                return false;
+        }
+        else if(s.charAt(0) == t.charAt(0)){
+            return isOneEditDistance(s.substring(1), t.substring(1));
+        }
+        else{
+            return s.equals(t.substring(1)) || t.equals(s.substring(1)) || s.substring(1).equals(t.substring(1));
+        }
+    }*/
+}
