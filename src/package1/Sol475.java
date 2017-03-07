283b12dfb6c6fdb6587d9711946ac9fea3c8686b
wyun <wyawxy@gmail.com>
2017-03-07 20:20:15 +0800

    Heaters

---
+++ src/package1/Sol475.java
@@ -0,0 +1,22 @@
+package package1;
+
+import java.util.*;
+
+public class Sol475 {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        
+        int i = 0, j = 0, res = 0;
+        while (i < houses.length) {
+            while (j < heaters.length - 1
+                && Math.abs(heaters[j + 1] - houses[i]) <= Math.abs(heaters[j] - houses[i])) {
+                j++;
+            }
+            res = Math.max(res, Math.abs(heaters[j] - houses[i]));
+            i++;
+        }
+        
+        return res;
+    }
+}
