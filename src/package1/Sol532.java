240fea78402b6b3693a9853290737cdfc3475e81
wyun <wyawxy@gmail.com>
2017-04-09 13:14:59 +0800

    K-diff Pairs in an Array

--- src/package1/Sol532.java
+++ src/package1/Sol532.java
@@ -1,5 +1,31 @@
 package package1;
 
-public class Sol532 {
+import java.util.*;
 
+public class Sol532 {
+    public int findPairs(int[] nums, int k) {
+        int p1 = 0, p2 = 1;
+        int ret = 0;
+        
+        Arrays.sort(nums);        //sort
+        
+        while(p1 < nums.length  && p2 < nums.length){
+            while(p2 < nums.length && nums[p2] < nums[p1] + k){
+                p2++;
+            }
+            if(p2 < nums.length && nums[p2] == nums[p1] + k){
+                ret++;
+                while(p1 < nums.length && nums[p2] == nums[p1] + k){
+                    p1++;
+                }
+                p2 = p1 + 1;
+            }
+            else{
+                p1++;
+                p2 = p1+1;
+            }
+        }
+        
+        return ret;
+    }
 }
