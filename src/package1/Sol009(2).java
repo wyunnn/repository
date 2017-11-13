bf4cb2bf63e6d4e686d74710bde920b6db5e0ff4
wyun <wyawxy@gmail.com>
2017-11-13 06:06:14 -0600

    9. Palindrome Number

---
+++ src/package1/Sol009(2).java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String a = Integer.toString(x);
+        int i = 0, j = a.length()-1;
+        
+        while(i < j){
+            if (a.charAt(i) == a.charAt(j)){
+                i++;
+                j--;
+            }
+            else 
+                return false;
+        }
+        return true;
+    }
+}
